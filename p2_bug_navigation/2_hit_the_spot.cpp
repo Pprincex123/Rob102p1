da9119a34039aafc7917aa8bcaeea0ae125650fa
Louie <pprincex@umich.edu>
2025-10-08 14:33:36 -0400

    hi

--- p2_bug_navigation/2_hit_the_spot.cpp
+++ p2_bug_navigation/2_hit_the_spot.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 #include <mbot_bridge/robot.h>
-
+#include <signal.h>
 #include <mbot_lib/behaviors.h>
 #include <mbot_lib/controllers.h>
 #include <mbot_lib/utils.h>
@@ -16,6 +16,7 @@ void ctrlc(int)
 
 
 int main(){
+signal(SIGINT, ctrlc);
     // Initialize the robot.
     mbot_bridge::MBot robot;
     // Reset the robot odometry to zero at the beginning of the run.
@@ -50,10 +51,10 @@ robot.drive(drive[0], drive[1], drive[2]);
     robot.stop();
 
     // *** Task: Print out the robot's final odometry pose *** //
-    vector<float>Final_odemetry_pose = robot.readOdemetry();
-    cout << Final_odemetry_pose[0];
-    cout << Final_odemetry_pose[1];
-    cout << Final_odemetry_pose[2];
+    vector<float>Final_odometry_pose = robot.readOdometry();
+    cout << Final_odometry_pose[0] << ", ";
+    cout << Final_odometry_pose[1] << ", ";
+    cout << Final_odometry_pose[2];
     // *** End student code *** //
 
     return 0;
